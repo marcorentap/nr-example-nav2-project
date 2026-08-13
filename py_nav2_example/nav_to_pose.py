19bb9d738ff4b7c153bc58db8b489cd79c50959a
Marc Owen Rentap <17574533+marcorentap@users.noreply.github.com>
2026-08-13 14:26:48 +0800

    Return to initial position

--- py_nav2_example/nav_to_pose.py
+++ py_nav2_example/nav_to_pose.py
@@ -13,7 +13,7 @@ def main():
     goal_pose = PoseStamped()
     goal_pose.header.frame_id = 'map'
     goal_pose.header.stamp = navigator.get_clock().now().to_msg()
-    goal_pose.pose.position.x = -1.0
+    goal_pose.pose.position.x = 0.0
     goal_pose.pose.position.y = 0.0
     goal_pose.pose.orientation.z = 0.0
     goal_pose.pose.orientation.w = 1.0
